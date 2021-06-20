401b25688140c1131114b5b31839b648a990b0cd
EdwinMiky96 <miky.apaza.y96gmail.com>
2021-06-20 17:45:26 -0500

    Ejemplo SubPrograma

---
+++ ProyectoSVM/src/App.java
@@ -0,0 +1,11 @@
+import pe.edu.upeu.SubProgramass;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+        SubProgramass objSP=new SubProgramass();
+        objSP.calcularFuncionExponecial();
+
+    }
+}
